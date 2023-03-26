2b007236b22c9d05c13ff0fcf757bdcbcc4c0db5
luozhy88 <94740863+luozhy88@users.noreply.github.com>
2023-03-26 17:35:06 +0800

    Update 01_install_miniconda.sh

--- install/01_install_miniconda.sh
+++ install/01_install_miniconda.sh
@@ -2,6 +2,7 @@
 ##################################01 create miniconda##################################
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+# wget https://repo.anaconda.com/miniconda/Miniconda3-py39_23.1.0-1-MacOSX-x86_64.sh  -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
